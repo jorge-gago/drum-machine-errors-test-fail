a8f896365bd31fc216819df34c940f675db8d6c0
jorgegago <jorgegago2526@gmail.com>
2023-01-15 17:36:14 +0100

    drum-machine

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { Pad } from './components/DrumPads/Drumpads';
+import { Display } from './components/DrumDisplay/DrumDisplay';
+import { useRef } from 'react';
 
 function App() {
+
+  const buttonsRefs = useRef([])
+  buttonsRefs.current = []
+
+  const audios = (url = "#") => {
+    let sound = new Audio(url)
+    return sound
+  }
+
+  const Handlerkey = (e) => {
+    console.log('key')
+    console.log(e.key.toUpperCase())
+    buttonsRefs.current.useImperativeHandle()
+  }
+
+  const addButtonRefs = (ref) => {
+    // console.log(ref)
+    if (ref && !buttonsRefs.current.includes(ref)) {
+      buttonsRefs.current.push(ref)
+    }
+    console.log(buttonsRefs.current)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="drum-machine" tabIndex="0" className="App" onKeyDown={(e) => {Handlerkey(e)}}>
+      {/* <Pad id={actions.pad1.letra} texto={actions.pad1.letra} audio={audios(actions.pad1.audio)}/> */}
+      {actions.map(pad => {
+        return(
+          <Pad key={pad.letra} id={pad.letra} texto={pad.letra} audio={audios(pad.audio)} ref={addButtonRefs(pad)}/>
+        )
+      })}
+      <Display />
+
     </div>
   );
 }
 
 export default App;
+
+
+const actions = [
+  { nombre:"Heater 1", letra: "Q", audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3" },
+  { nombre:"Heater 2", letra: "W", audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3" },
+  { nombre:"Heater 3", letra: "E", audio: "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3" }
+]
---
+++ src/components/DrumPads/Drumpads.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Style from "./DrumPads.module.css"
+//import { useEffect, useState } from "react";
+
+export const Pad = ({ id = null, texto = 'place holder', audio = null, estado = false}) => {
+    //const [ activo, setActivo] = useState(estado)
+
+    // useEffect(() => {
+    //     console.log("inside")
+    //     audio.play() || console.log("effect ")
+  
+    // } ,[activo])
+    const useImperativeHandle = () => {
+        playSound()
+    }
+
+    const clickHandler = () => {
+        //return audio? audio.play() : console.log('no function')
+        playSound()
+    }
+
+    const playSound = () =>{
+        return audio? audio.play() : console.log('no function')
+    }
+
+
+
+    return (
+        <div id = {id}  className = {`${Style.pad}`} onClick={() => clickHandler()}>
+            {texto}
+        </ div>
+    )
+}
