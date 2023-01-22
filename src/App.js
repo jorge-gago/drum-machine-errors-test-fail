4962a4f6061a205adfafd2a8f6f0f138923cc3cc
jorgegago <jorgegago2526@gmail.com>
2023-01-22 11:13:51 +0100

    cambios test 6 fails

--- src/App.js
+++ src/App.js
@@ -1,36 +1,40 @@
 import './App.css';
 import { Pad } from './components/DrumPads/Drumpads';
 import { Display } from './components/DrumDisplay/DrumDisplay';
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
 function App() {
 
   const buttonsRefs = useRef({})
   buttonsRefs.current = {}
 
-  const audios = (url = "#") => {
-    let sound = new Audio(url)
-    return sound
-  }
+  useEffect(() => {
+    document.getElementById('drum-machine').focus()
+  }, [])
+
+  // const audios = (url = "#") => {
+  //   let sound = new Audio(url)
+  //   return sound
+  // }
 
   const Handlerkey = (e) => {
-    console.log('key')
-    console.log(e.key.toUpperCase())
+    console.log(e)
+    // console.log(e.key.toUpperCase())
     buttonsRefs.current[e.key.toUpperCase()](
-      () => { return(true)}
-    )
+    () => { return(true)}
+    ) 
     // console.log(buttonsRefs.current[e.key.toUpperCase()])
   }
 
   const addButtonRefs = (ref) => {
-    console.log('add ')
+    // console.log('add ')
     //console.log("ref ", ref.letra, ref)
     if (ref && !buttonsRefs.current[ref.letra]) {
       // buttonsRefs.current[ref.letra]
       //buttonsRefs.current.push(ref)
       buttonsRefs.current[ref[1]] = ref[0]
     }
-    console.log(buttonsRefs.current)
+    // console.log(buttonsRefs.current)
   }
 
 
@@ -38,11 +42,23 @@ function App() {
     <div autoFocus id="drum-machine"  tabIndex="0" className="App" onKeyDown={(e) => {Handlerkey(e)}}>
       {/* <Pad id={actions.pad1.letra} texto={actions.pad1.letra} audio={audios(actions.pad1.audio)}/> */}
       <span className='botones'>
-      {actions.map(pad => {
-        return(
-          <Pad key={pad.letra} id={pad.letra} texto={pad.letra} audio={audios(pad.audio)} ref={addButtonRefs}/>
-        )
-      })}
+      <Pad  id={actions[0].letra} texto={actions[0].letra} audio={actions[0].audio} ref={addButtonRefs}/>
+
+      <Pad  id={actions[1].letra} texto={actions[1].letra} audio={actions[1].audio} ref={addButtonRefs}/>
+
+      <Pad  id={actions[2].letra} texto={actions[2].letra} audio={actions[2].audio} ref={addButtonRefs}/>
+
+      <Pad  id={actions[3].letra} texto={actions[3].letra} audio={actions[3].audio} ref={addButtonRefs}/>
+
+      <Pad  id={actions[4].letra} texto={actions[4].letra} audio={actions[4].audio} ref={addButtonRefs}/>
+
+      <Pad  id={actions[5].letra} texto={actions[5].letra} audio={actions[5].audio} ref={addButtonRefs}/>
+
+      <Pad  id={actions[6].letra} texto={actions[6].letra} audio={actions[6].audio} ref={addButtonRefs}/>
+
+      <Pad  id={actions[7].letra} texto={actions[7].letra} audio={actions[7].audio} ref={addButtonRefs}/>
+
+      <Pad id={actions[8].letra} texto={actions[8].letra} audio={actions[8].audio} ref={addButtonRefs}/>
       </span>
       <Display  />
 
@@ -64,3 +80,14 @@ const actions = [
   { nombre:"kick", letra: "X", audio: "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3" },
   { nombre:"close-HH", letra: "C", audio: "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3" },
 ]
+
+
+
+
+{/* <Pad key={pad.letra} id={pad.letra} texto={pad.letra} audio={audios(pad.audio)} ref={addButtonRefs}/> */}
+
+// actions.map(pad => {
+//   return(
+//     <Pad key={pad.letra} id={pad.letra} texto={pad.letra} audio={pad.audio} ref={addButtonRefs}/>
+//   )
+// })}
--- src/components/DrumPads/Drumpads.js
+++ src/components/DrumPads/Drumpads.js
@@ -2,11 +2,11 @@ import React, {useImperativeHandle, forwardRef} from "react";
 import Style from "./DrumPads.module.css"
 import { useEffect, useState } from "react";
 
-export const Pad = forwardRef(({ id = null, texto = 'place holder', audio = null, estado = false}, ref) => {
+export const Pad = forwardRef(({ id = null, texto = 'place holder', audio = "#", estado = false}, ref) => {
     const [ activo, setActivo] = useState(estado)
 
     useEffect(() => {
-        console.log("inside ", id)
+        // console.log("inside ", id)
         playSound()
         setActivo(() => {return false})
     } , [activo])
@@ -21,14 +21,18 @@ export const Pad = forwardRef(({ id = null, texto = 'place holder', audio = null
     }
 
     const playSound = () =>{
-        return audio? audio.play() : console.log('no function')
+        //return audio? audio.play() : console.log('no function')
+        document.getElementById(id).pause()
+        document.getElementById(id).currentTime = 0
+        document.getElementById(id).play()
     }
 
 
 
     return (
-        <div id = {id} className = {`${Style.pad} drum-pad` } onClick={() => clickHandler()}>
+        <div id = {`pad-${id}`} className = {`${Style.pad} drum-pad` } onClick={() => clickHandler()}>
+            <audio id={id} src={audio} className='clip'></audio>
             {texto}
-        </ div>
+        </ div> 
     )
 })
